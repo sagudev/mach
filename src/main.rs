bcc1d56b9b3f4332e658cafb248508e6acd86068
sagudev <samo.golez@outlook.com>
2020-02-15 10:22:08 +0100

    r

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::prelude::*;
+use serde_json::Value;
+mod v0;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("mach.json").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    //println!("readed: {}", contents);
+    let json: Value = serde_json::from_str(&contents).unwrap();
+    let json = json.as_object().unwrap();
+    let version = json.get("version").unwrap().as_u64().unwrap();
+    println!("version: {:?}", version);
 }
